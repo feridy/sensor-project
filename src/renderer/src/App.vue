5c6d031cab9d6a87ca41359a902316b2312098b0
Feridy <xliu16@outlook.com>
2024-11-16 13:52:29 +0800

    feat: 🎸 进行代码保存

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,13 +4,12 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 import { ref } from 'vue';
 import Main from './components/Main.vue';
-import D3 from './components/D3.vue';
 dayjs.locale('zh-cn');
 const locale = ref(zhCN);
 </script>
 
 <template>
   <a-config-provider :locale="locale">
-    <D3 />
+    <Main />
   </a-config-provider>
 </template>
